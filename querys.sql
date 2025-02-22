8203dd9e1a2d18db490e56969924d644bd58fe9c
Carlox101 <carlos.hs29665@gmail.com>
2025-02-22 19:37:41 -0400

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -1,1 +1,14 @@
 -- Consultas
+-- CONSULTA D
+/*
+Esta consulta obtiene un reporte completo de empleados que cumplen con al menos una de estas características:
+- Han vendido productos en más de 1 sucursal.
+- Trabajan en la misma sucursal que su supervisor.
+- Tienen un cargo que se encuentra entre los 5 mejores pagados de la empresa.
+
+El reporte incluye:
+- Información de empleado (CI, nombre completo, sexo).
+- Información del cargo (nombre).
+- Información de su sueldo detallada (salario base por su cargo por hora, bono fijo mensual, total por mes que obtiene el empleado dadas sus horas y días trabajados y monto total recibido desde 
+  el inicio de su contrato).
+*/
