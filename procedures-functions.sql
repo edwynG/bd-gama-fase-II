325635e1162e84ec3f3d5b04e31e2be059a0d0c6
Edwyn Guzman <edwynjesus30@gmail.com>
2025-03-06 07:02:38 -0400

    fix - procedure E

--- procedures-functions.sql
+++ procedures-functions.sql
@@ -17,46 +17,82 @@ BEGIN
     FROM Promo p 
     WHERE p.id = @id;
 
-    -- Calcular la media de ingresos y monto total desde 3 meses antes de la promoción
+    -- Calcular la media de la cantidad de ventas y monto total desde 3 meses antes de la promoción
     SELECT 
-        @mediaAntes = AVG(f.montoTotal), 
+        @mediaAntes = 
+        (  SELECT SUM(Temp.cantidad)
+            FROM (
+                SELECT SUM(fd.cantidad) AS cantidad
+                FROM Factura f2
+                JOIN FacturaDetalle fd ON fd.facturaId = f2.id
+                WHERE f2.fechaEmision BETWEEN DATEADD(MONTH, -3, @fechaInicio) AND DATEADD(DAY, -1, @fechaInicio)
+                GROUP BY f2.id
+            ) AS Temp
+        ) / COUNT(*), 
         @ingresoTotalAntes = SUM(f.montoTotal) 
     FROM Factura f 
-    WHERE f.fechaEmision BETWEEN DATEADD(MONTH, -3, @fechaInicio) AND @fechaInicio;
+    WHERE f.fechaEmision BETWEEN DATEADD(MONTH, -3, @fechaInicio) AND DATEADD(DAY, -1, @fechaInicio);
 
-    -- Calcular la media de ingresos y monto total durante la promoción
+    -- Calcular la media de cantidad de ventas y monto total durante la promoción
     SELECT 
-        @mediaDurante = AVG(f.montoTotal), 
+        @mediaDurante = 
+        (  SELECT SUM(Temp.cantidad)
+            FROM (
+                    SELECT SUM(fd.cantidad) AS cantidad
+                    FROM Factura f3
+                    JOIN FacturaDetalle fd ON fd.facturaId = f3.id
+                    WHERE (f3.fechaEmision BETWEEN @fechaInicio AND @fechaFin) AND 
+                                f3.id IN
+                                ( 
+                                    SELECT fp2.facturaId 
+                                    FROM Promo p2
+                                    JOIN FacturaPromo fp2 ON fp2.promoId = p2.id
+                                    JOIN Factura f4 ON f4.id = fp2.facturaId
+                                    WHERE p2.id = @id
+                                    GROUP BY fp2.facturaId
+                                ) 
+                    GROUP BY f3.id
+            ) AS Temp
+        ) / COUNT(*), 
         @ingresoTotalDurante = SUM(f.montoTotal) 
     FROM Factura f 
-    WHERE f.fechaEmision BETWEEN @fechaInicio AND @fechaFin;
+    WHERE (f.fechaEmision BETWEEN @fechaInicio AND @fechaFin) AND 
+            f.id IN
+            ( 
+                SELECT fp.facturaId 
+                FROM Promo p
+                JOIN FacturaPromo fp ON fp.promoId = p.id
+                JOIN Factura f2 ON f2.id = fp.facturaId
+                WHERE p.id = @id
+                GROUP BY fp.facturaId
+            ) ;
 
     -- Calcular el porcentaje de cambio
-    IF @mediaAntes IS NULL OR @mediaDurante IS NULL
+    IF @mediaAntes IS NULL AND @mediaDurante IS NULL
     BEGIN
         SELECT 'No hay datos suficientes para calcular la efectividad de la promoción.' AS Mensaje;
         RETURN;
     END
 
-    IF @mediaDurante < @mediaAntes
+    IF @ingresoTotalDurante < @ingresoTotalAntes
     BEGIN
-        SET @porcentaje = ((@mediaAntes - @mediaDurante) / @mediaAntes) * 100;
+        SET @porcentaje = ((@ingresoTotalAntes - @ingresoTotalDurante) / @ingresoTotalAntes) * 100;
         SELECT 
-            @mediaAntes AS MediaAntes, 
-            @ingresoTotalAntes AS IngresoTotalAntes, 
-            @mediaDurante AS MediaDurante, 
-            @ingresoTotalDurante AS IngresoTotalDurante, 
-            @porcentaje AS "Porcentaje de Decremento";
+            COALESCE(@mediaAntes, 0) AS MediaAntes, 
+            COALESCE(@ingresoTotalAntes, 0) AS IngresoTotalAntes, 
+            COALESCE(@mediaDurante, 0) AS MediaDurante, 
+            COALESCE(@ingresoTotalDurante, 0) AS IngresoTotalDurante, 
+            COALESCE(@porcentaje, 0) AS "Porcentaje de decremento";
     END
     ELSE
     BEGIN
-        SET @porcentaje = ((@mediaDurante - @mediaAntes) / @mediaAntes) * 100;
+        SET @porcentaje = ((@ingresoTotalDurante - @ingresoTotalAntes) / @ingresoTotalAntes) * 100;
         SELECT 
-            @mediaAntes AS MediaAntes, 
-            @ingresoTotalAntes AS IngresoTotalAntes, 
-            @mediaDurante AS MediaDurante, 
-            @ingresoTotalDurante AS IngresoTotalDurante, 
-            @porcentaje AS "Porcentaje de Incremento";
+            COALESCE(@mediaAntes, 0) AS MediaAntes, 
+            COALESCE(@ingresoTotalAntes, 0) AS IngresoTotalAntes, 
+            COALESCE(@mediaDurante, 0) AS MediaDurante, 
+            COALESCE(@ingresoTotalDurante, 0) AS IngresoTotalDurante, 
+            COALESCE(@porcentaje, 0) AS "Porcentaje de incremento";
     END
 END;
 
