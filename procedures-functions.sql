f68fd6436ffc4f038fdc0a4c3c14511cc9d4c912
Edwyn Guzman <edwynjesus30@gmail.com>
2025-03-06 02:44:50 -0400

    Implemented - function B

--- procedures-functions.sql
+++ procedures-functions.sql
@@ -10,8 +10,8 @@ BEGIN
     DECLARE @mediaAntes DECIMAL(10, 2);
     DECLARE @mediaDurante DECIMAL(10, 2);
     DECLARE @porcentaje DECIMAL(10, 2);
-	DECLARE @ingresoTotalAntes MONEY;
-	DECLARE @ingresoTotalDurante MONEY;
+	DECLARE @ingresoTotalAntes DECIMAL(10, 2);
+	DECLARE @ingresoTotalDurante DECIMAL(10, 2);
     -- Fechas de la promoción
     SELECT @fechaInicio = p.fechaInicio, @fechaFin = p.fechaFin 
     FROM Promo p 
@@ -63,3 +63,45 @@ END;
 -- Implementacion de funciones
 -- Parte II
 --- Function B
+CREATE FUNCTION isValidPromo (@facturaId INT, @promoId INT)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @isValid BIT;
+    SET @isValid = 0; -- Inicialmente se asume que no es válida
+	 -- Obtener la promoción
+    DECLARE @tipoPromocion VARCHAR(15);
+    DECLARE @fechaFin DATE;
+   	DECLARE @fechaInicio DATE;
+    DECLARE @fechaEmision DATETIME;
+
+   	-- Obtenemos los datos necesarios de la promo
+    SELECT @tipoPromocion = p.tipoPromocion, @fechaFin = p.fechaFin, @fechaInicio = fechaInicio 
+    FROM Promo p
+    WHERE p.id = @promoId;
+   -- Obtenemos la fecha de emisión de la factura
+    SELECT @fechaEmision = f.fechaEmision
+	FROM Factura f 
+	WHERE f.id = @facturaId;
+
+    -- Verificar si la factura es online
+    IF EXISTS (SELECT 1 FROM OrdenOnline oo WHERE oo.facturaId = @facturaId)
+    BEGIN
+        -- Verificar si la fecha de fin no ha pasado y el tipo de promoción es 'Online'
+        IF (CAST(@fechaEmision AS DATE) BETWEEN @fechaInicio AND @FechaFin)  AND @tipoPromocion IN ('Online', 'Ambos')
+        BEGIN
+            SET @isValid = 1; -- La promoción es válida
+        END
+    END
+    ELSE
+    BEGIN
+        -- De lo contrario, es Fisica
+        -- Verificar si la fecha de fin no ha pasado y el tipo de promoción es 'Fisica'
+        IF (CAST(@fechaEmision AS DATE) BETWEEN @fechaInicio AND @FechaFin)  AND @tipoPromocion IN ('Fisica', 'Ambos')
+        BEGIN
+            SET @isValid = 1; -- La promoción es válida
+        END
+    END
+   
+    RETURN @isValid; -- Retorna el resultado
+END;
